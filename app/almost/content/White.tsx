99e0a87f5f214a8744fb065f64bf277b7539815a
LowTicket8D <laurapereiraofc576@gmail.com>
2026-03-13 14:14:30 -0300

    mudei botao white

--- app/almost/content/White.tsx
+++ app/almost/content/White.tsx
@@ -116,7 +116,7 @@ export default function White() {
                   transition={{ delay: 0.7 }}
                   className="pt-4"
                 >
-                <PerfectPayUpsell upsellCheckoutUrl={UPSELL_1_CHECKOUT_URL} skipUrl="/thanks" skipLabel="No thanks, take me to my area" black={false} />
+                <PerfectPayUpsell upsellCheckoutUrl={UPSELL_1_CHECKOUT_URL} ctaText="Yes, I want lifetime access - $17" skipUrl="/thanks" skipLabel="No thanks, take me to my area" black={false} />
               </motion.div>
 
               {/* Trust Indicators */}
